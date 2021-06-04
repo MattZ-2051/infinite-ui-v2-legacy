4c543183dfc7ba227e95f365ec4fb2f12d9b0e87
Tasos Kotsopoulos <tkotsopoulos@gmail.com>
2021-06-04 17:00:28 +0300

    feat: add time countdown

--- src/lib/sku-item/TimeDifference.svelte
+++ src/lib/sku-item/TimeDifference.svelte
@@ -1,32 +1,13 @@
 <script lang="ts" context="module">
-  function timeRemaining(_date: Date) {
-    let now = +new Date();
-
-    let delta = Math.abs(+_date - now) / 1000;
-
-    // calculate (and subtract) whole days
-    let days = Math.floor(delta / 86400);
-    delta -= days * 86400;
-
-    // calculate (and subtract) whole hours
-    let hours = Math.floor(delta / 3600) % 24;
-    delta -= hours * 3600;
-
-    // calculate (and subtract) whole minutes
-    let minutes = Math.floor(delta / 60) % 60;
-    delta -= minutes * 60;
-
-    // what's left is seconds
-    let seconds = Math.floor(delta % 60);
-    console.log(days, hours, minutes, seconds);
-    return [days, hours, minutes, seconds];
-  }
+  import { time } from './clock';
 </script>
 
 <script lang="ts">
+  import { timeRemaining } from './clock';
+  // let now = new Date();
   export let date: Date;
-  const difference = timeRemaining(date);
+  $: difference = timeRemaining(date, $time);
 </script>
 
-<!-- {difference[0]}d,,{difference[3]} s -->
-{difference[1]}h {difference[2]}m
+<!-- {difference[0]}d,, -->
+{difference[1]}h {difference[2]}m {difference[3]}s
---
+++ src/lib/sku-item/clock.ts
@@ -0,0 +1,31 @@
+import { readable } from 'svelte/store';
+
+export const time = readable(new Date(), function start(set) {
+  const interval = setInterval(() => {
+    set(new Date());
+  }, 1000);
+
+  return function stop() {
+    clearInterval(interval);
+  };
+});
+export function timeRemaining(_date: Date, now: Date): number[] {
+  let delta = Math.abs(+_date - +now) / 1000;
+
+  // calculate (and subtract) whole days
+  const days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+
+  // calculate (and subtract) whole hours
+  const hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+
+  // calculate (and subtract) whole minutes
+  const minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+
+  // what's left is seconds
+  const seconds = Math.floor(delta % 60);
+
+  return [days, hours, minutes, seconds];
+}
