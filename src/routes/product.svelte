5cbb15fa1dba1aea072b0e1eca579dd06a610219
Tasos Bekos <tbekos@gmail.com>
2021-06-07 17:51:20 +0300

    fix(product): changes to align with `container`

--- src/routes/product.svelte
+++ src/routes/product.svelte
@@ -6,10 +6,10 @@
 <div class="bg-gray-900">
   <div class="container py-0">
     <div class="flex flex-col flex-auto lg:flex-row">
-      <div class="lg:max-w-md">
+      <div class="lg:max-w-md -container-x lg:-container-none">
         <ProductDetails />
       </div>
-      <div class="flex-grow pt-6 px-6 pb-16 lg:pl-16 lg:pr-0 lg:pb-0">
+      <div class="flex-grow pt-6 pl-0 pb-16 lg:pl-16 lg:pb-0">
         <ProductSummary />
       </div>
     </div>
