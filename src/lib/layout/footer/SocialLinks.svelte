ed3dc4b4e294b23172453346959100c6e118eeab
Tasos Bekos <tbekos@gmail.com>
2021-06-22 17:58:11 +0300

    feat(app): add correct social links

---
+++ src/lib/layout/footer/SocialLinks.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { mdiTwitter, mdiYoutube, mdiInstagram, mdiLinkedin, mdiFacebook } from '@mdi/js';
+  import Icon from '$ui/icon/Icon.svelte';
+
+  export let size = 1.33;
+
+  const links = [
+    {
+      id: 'Youtube',
+      href: 'https://www.youtube.com/channel/UCsSdn6MM2hZir_s70yO0C4w/',
+      path: mdiYoutube,
+    },
+    {
+      id: 'Facebook',
+      href: 'https://www.facebook.com/The-ARIA-Network-106228618351483',
+      path: mdiFacebook,
+    },
+    { id: 'Twitter', href: 'https://twitter.com/TheAriaNetwork', path: mdiTwitter },
+    { id: 'Instagram', href: 'https://www.instagram.com/TheAriaNetwork/', path: mdiInstagram },
+    { id: 'LinkedIn', href: 'https://www.linkedin.com/company/vrm-aria', path: mdiLinkedin },
+  ];
+</script>
+
+{#each links as link (link.id)}
+  <a href={link.href}
+    ><Icon
+      path={link.path}
+      {size}
+      class="flex justify-center items-center px-2 rounded-xl text-white bg-gray-800 hover:bg-white hover:text-black"
+    /><span class="sr-only">{link.id}</span></a
+  >
+{/each}
