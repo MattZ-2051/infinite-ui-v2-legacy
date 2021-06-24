8c2dc9b92fb4504420508943d95f97f298f3fe38
Tasos Bekos <tbekos@gmail.com>
2021-06-24 23:13:06 +0300

    feat(util): add decimal format

--- src/util/format.test.ts
+++ src/util/format.test.ts
@@ -1,7 +1,13 @@
-import { formatCurrency } from './format';
+import { formatCurrency, formatDecimal } from './format';
 
 describe('format', () => {
   it('currency', () => {
     expect(formatCurrency(123_456.789)).toEqual('$123,456.79');
+  });
+
+  it('decimal', () => {
+    expect(
+      formatDecimal(123_456.789, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+    ).toEqual('123,456.79');
   });
 });
--- src/util/format.ts
+++ src/util/format.ts
@@ -1,3 +1,9 @@
-export function formatCurrency(value: number) {
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
+export function formatCurrency(value: number, options?: Intl.NumberFormatOptions) {
+  return new Intl.NumberFormat('en-US', { ...options, style: 'currency', currency: 'USD' }).format(
+    value
+  );
+}
+
+export function formatDecimal(value: number, options?: Intl.NumberFormatOptions) {
+  return new Intl.NumberFormat('en-US', { ...options, style: 'decimal' }).format(value);
 }
