aaecb40d6cd15e062c1535972d6d1d4e3587d095
Tasos Bekos <tbekos@gmail.com>
2021-07-03 01:34:07 +0300

    feat(comp): add `Button` themes

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,22 +4,38 @@
 
   export let type = 'button';
   export let disabled = false;
+  export let theme: 'primary' | 'secondary' = 'primary';
 
   let _class = '';
   export { _class as class };
 
   const forwardEvents = forwardEventsBuilder(get_current_component());
+
+  const themes = {
+    primary: {
+      default: 'bg-black text-white transform duration-200 outline-none',
+      hover: 'hover:scale-105',
+    },
+    secondary: {
+      default: 'bg-white text-black border-2 border-black transform duration-200',
+      hover: 'hover:bg-black hover:text-white hover:scale-105',
+    },
+  };
+
+  $: _theme = themes[theme];
 </script>
 
 <button
   {type}
   use:forwardEvents
   {disabled}
-  class="bg-black {disabled
-    ? 'opacity-50 cursor-default'
-    : 'hover:bg-gray-900'} text-white text-xl text-center rounded-full p-4 w-full disabled:opacity-50 {_class || ''}"
+  class="{_theme.default} {!disabled && _theme.hover} text-xl text-center rounded-full p-4 w-full {_class || ''}"
   {...$$restProps}><slot /></button
 >
 
-<style>
+<style lang="postcss">
+  button:disabled,
+  button[disabled] {
+    @apply opacity-50 cursor-default;
+  }
 </style>
