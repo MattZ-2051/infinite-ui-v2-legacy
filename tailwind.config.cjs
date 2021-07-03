58c39d2316a9da99eb5282a79f3da18dc393fb2c
Tasos Bekos <tbekos@gmail.com>
2021-07-03 19:36:14 +0300

    fix(repo): adjust Tailwind extractor for `class:*`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,19 @@
+const { tailwindExtractor } = require('tailwindcss/lib/lib/purgeUnusedStyles');
+
 module.exports = {
   mode: 'aot', // jit
-  purge: ['./src/**/*.{html,js,svelte,ts}'],
+  purge: {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    options: {
+      defaultExtractor: (content) => [
+        // If this stops working, please open an issue at https://github.com/svelte-add/svelte-add/issues rather than bothering Tailwind Labs about it
+        ...tailwindExtractor(content),
+        // Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+        ...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+      ],
+    },
+    safelist: [/^svelte-[\d\w]+$/],
+  },
   corePlugins: {
     container: false,
   },
