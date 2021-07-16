b72e0cc4000d07ba80780792fd2bf61eaef48c69
Tasos Bekos <tbekos@gmail.com>
2021-07-17 01:35:31 +0300

    fix(app): handle empty series name on product info modal

--- src/lib/features/product/ProductModalInfo.svelte
+++ src/lib/features/product/ProductModalInfo.svelte
@@ -3,6 +3,7 @@
   import IconRedeem from '$lib/sku-item/IconRedeem.svelte';
   import Rarity from '$lib/rarity/Rarity.svelte';
   import { formatCurrency } from '$util/format';
+  import { Breadcrumb, BreadcrumbItem } from '$ui/breadcrumbs';
 
   export let sku: Sku;
   export let price: number = undefined;
@@ -25,16 +26,21 @@
     {/if}
   </div>
   <div class="flex">
-    <span class="text-gray-400 font-medium">{sku.series.name}</span>
-    {#if sku.redeemable}
-      <span class="text-gray-400 font-medium px-2">/</span>
-      <IconRedeem class="text-black bg-white rounded-full mr-1 p-1 border" />
-      <span>Redeemable</span>
-    {/if}
+    <Breadcrumb>
+      {#if sku.series?.name}
+        <BreadcrumbItem class="text-gray-500 font-medium">{sku.series.name}</BreadcrumbItem>
+      {/if}
+      {#if sku.redeemable}
+        <BreadcrumbItem>
+          <IconRedeem class="text-black bg-white rounded-full mr-1 p-1 border" />
+          <span>Redeemable</span>
+        </BreadcrumbItem>
+      {/if}
+    </Breadcrumb>
   </div>
   <div class="text-right">
     {#if serial}
-      <span class="font-bold italic text-gray-400">Serial:</span>
+      <span class="font-bold italic text-gray-500">Serial:</span>
       <span class="font-bold">#{serial}</span>
     {/if}
   </div>
