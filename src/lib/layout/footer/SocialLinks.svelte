13efdbf82806409f85db15a25a8befbd645d34fb
Tasos Bekos <tbekos@gmail.com>
2021-07-28 14:22:53 +0300

    fix(app): use correct social links

--- src/lib/layout/footer/SocialLinks.svelte
+++ src/lib/layout/footer/SocialLinks.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-  import { mdiTwitter, mdiYoutube, mdiInstagram, mdiLinkedin, mdiFacebook, mdiDiscord, mdiEmail } from '@mdi/js';
+  import { mdiTwitter, mdiInstagram, mdiDiscord, mdiEmail } from '@mdi/js';
   import Icon from '$ui/icon/Icon.svelte';
 
   export let size = 1.33;
 
   const links = [
-    {
-      id: 'Youtube',
-      href: 'https://www.youtube.com/channel/UCsSdn6MM2hZir_s70yO0C4w/',
-      path: mdiYoutube,
-    },
-    {
-      id: 'Facebook',
-      href: 'https://www.facebook.com/The-ARIA-Network-106228618351483',
-      path: mdiFacebook,
-    },
-    { id: 'Twitter', href: 'https://twitter.com/TheAriaNetwork', path: mdiTwitter },
-    { id: 'Instagram', href: 'https://www.instagram.com/TheAriaNetwork/', path: mdiInstagram },
-    { id: 'LinkedIn', href: 'https://www.linkedin.com/company/vrm-aria', path: mdiLinkedin },
+    // {
+    //   id: 'Youtube',
+    //   href: 'https://www.youtube.com/channel/UCsSdn6MM2hZir_s70yO0C4w/',
+    //   path: mdiYoutube,
+    // },
+    // {
+    //   id: 'Facebook',
+    //   href: 'https://www.facebook.com/The-ARIA-Network-106228618351483',
+    //   path: mdiFacebook,
+    // },
+    { id: 'Twitter', href: 'https://twitter.com/TheAriaExchange', path: mdiTwitter },
+    { id: 'Instagram', href: 'https://www.instagram.com/theariaexchange/', path: mdiInstagram },
+    // { id: 'LinkedIn', href: 'https://www.linkedin.com/company/vrm-aria', path: mdiLinkedin },
     { id: 'Discord', href: 'https://discord.gg/3JtgwbduBt', path: mdiDiscord },
     { id: 'Email', href: 'mailto:support@ariaexchange.com', path: mdiEmail },
   ];
