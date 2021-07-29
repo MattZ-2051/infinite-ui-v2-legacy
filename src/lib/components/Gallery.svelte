f877d5eb3b50a68ec93d270853686f35ee7bb123
Morfo <mfragkop@gmail.com>
2021-07-29 12:29:01 +0300

    fix(app): thumbnails ux improvement

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -9,10 +9,10 @@
   $: activeItem = selectedItem || items?.[0];
 </script>
 
-<div class="flex relative justify-center items-center h-full text-white">
+<div class="flex flex-col gap-2 justify-center items-center h-full text-white">
   <FilePreview item={activeItem} />
-  {#if items?.length > 0}
-    <div class="absolute gap-2 bottom-10 left-5 flex">
+  {#if items?.length > 1}
+    <div class="gap-2 flex flex-wrap justify-center md:mb-2">
       {#each items as item}
         <div
           class="border cursor-pointer w-16 h-16 opacity-70 hover:opacity-100 {activeItem === item
