1363317a40ede6e64ac63ee56e9bca6b96a6dacf
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2021-08-05 13:33:57 +0300

    fix(app): preload font to prevent layout changes

--- scope/aria/src/Head.svelte
+++ scope/aria/src/Head.svelte
@@ -6,5 +6,6 @@
 <meta name="description" content={CLIENT_DOC_DESCRIPTION} />
 <link rel="preload" as="font" href="/fonts/Graphik/Graphik-Bold.woff2" type="font/woff2" crossorigin="anonymous" />
 <link rel="preload" as="font" href="/fonts/Graphik/Graphik-Regular.woff2" type="font/woff2" crossorigin="anonymous" />
+<link rel="preload" as="font" href="/fonts/Graphik/Graphik-Medium.woff2" type="font/woff2" crossorigin="anonymous" />
 <link rel="preload" as="font" href="/fonts/Graphik/Graphik-Light.woff2" type="font/woff2" crossorigin="anonymous" />
 <link rel="stylesheet" href="/fonts/Graphik/stylesheet.css" />
