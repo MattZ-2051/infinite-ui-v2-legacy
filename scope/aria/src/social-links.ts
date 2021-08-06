7dd14306cf5109e9bda0f53b90693ab8690670e8
Tasos Bekos <tbekos@gmail.com>
2021-08-06 17:19:42 +0300

    feat(app): add more social links for Aria

--- scope/aria/src/social-links.ts
+++ scope/aria/src/social-links.ts
@@ -1,19 +1,24 @@
-import { mdiTwitter, mdiInstagram, mdiDiscord, mdiEmail } from '@mdi/js';
+import { mdiTwitter, mdiInstagram, mdiDiscord, mdiFacebook, mdiLinkedin, mdiPinterest, mdiReddit } from '@mdi/js';
 
 export const links = [
-  // {
-  //   id: 'Youtube',
-  //   href: 'https://www.youtube.com/channel/UCsSdn6MM2hZir_s70yO0C4w/',
-  //   path: mdiYoutube,
-  // },
-  // {
-  //   id: 'Facebook',
-  //   href: 'https://www.facebook.com/The-ARIA-Network-106228618351483',
-  //   path: mdiFacebook,
-  // },
-  { id: 'Twitter', href: 'https://twitter.com/TheAriaExchange', path: mdiTwitter },
   { id: 'Instagram', href: 'https://www.instagram.com/theariaexchange/', path: mdiInstagram },
-  // { id: 'LinkedIn', href: 'https://www.linkedin.com/company/vrm-aria', path: mdiLinkedin },
-  { id: 'Discord', href: 'https://discord.gg/3JtgwbduBt', path: mdiDiscord },
-  { id: 'Email', href: 'mailto:support@ariaexchange.com', path: mdiEmail },
+  { id: 'Twitter', href: 'https://twitter.com/TheAriaExchange', path: mdiTwitter },
+  {
+    id: 'Facebook',
+    href: 'https://www.facebook.com/The-ARIA-Exchange-101775858801456',
+    path: mdiFacebook,
+  },
+  { id: 'LinkedIn', href: 'https://www.linkedin.com/company/the-aria-exchange/', path: mdiLinkedin },
+  {
+    id: 'Pinterest',
+    href: 'https://www.pinterest.com/TheARIAExchange',
+    path: mdiPinterest,
+  },
+  {
+    id: 'Reddit',
+    href: 'https://www.reddit.com/user/TheARIAExchange',
+    path: mdiReddit,
+  },
+  { id: 'Discord', href: 'https://discord.gg/SmJApard', path: mdiDiscord },
+  // { id: 'Email', href: 'mailto:support@ariaexchange.com', path: mdiEmail },
 ];
