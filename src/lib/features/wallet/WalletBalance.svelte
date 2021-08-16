19e991c36d194de19fc1859cb1f64c0828175bd4
Morfo <mfragkop@gmail.com>
2021-08-16 16:53:41 +0300

    fix(app): wallet deposit and withdraw buttons style changes

--- src/lib/features/wallet/WalletBalance.svelte
+++ src/lib/features/wallet/WalletBalance.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import { mdiArrowRight, mdiArrowLeft } from '@mdi/js';
   import { createEventDispatcher } from 'svelte';
   import { user } from '$lib/user';
   import { Tabs, Tab } from '$ui/tabs';
   import { formatCurrency } from '$util/format';
   import Button from '$lib/components/Button.svelte';
+  import Icon from '$ui/icon/Icon.svelte';
   import { withdrawableBalance } from './wallet.store';
 
   const dispatch = createEventDispatcher();
@@ -33,7 +35,10 @@
       <br />
       <span class="text-xs text-gray-400">(Excludes pending transactions and active bids)</span>
     </div>
-    <Button on:click={() => dispatch('deposit')}>Deposit</Button>
+    <Button class="flex justify-between" on:click={() => dispatch('deposit')}>
+      <span>Deposit</span>
+      <Icon path={mdiArrowRight} size="1.2" />
+    </Button>
     <div class="text-sm mt-4">
       <span class="text-gray-400 mr-1">Withdrawable:</span>
       {#if Number.isNaN($withdrawableBalance)}
@@ -47,11 +52,12 @@
       >
     </div>
     <Button
-      variant="secondary"
+      class="flex justify-between"
       disabled={!$withdrawableBalance || $withdrawableBalance === 0}
       on:click={() => dispatch('withdraw')}
     >
-      Withdraw
+      <span>Withdraw</span>
+      <Icon path={mdiArrowLeft} size="1.2" />
     </Button>
   </div>
 </div>
