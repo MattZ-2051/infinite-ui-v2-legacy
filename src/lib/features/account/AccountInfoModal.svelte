82d647223597b27524def83d37b1e8872549cfde
tkotsopoulos <tkotsopoulos@gmail.com>
2021-08-23 13:44:17 +0300

    feat(app): add toast messages on edit profile username errors
    Closes #151

--- src/lib/features/account/AccountInfoModal.svelte
+++ src/lib/features/account/AccountInfoModal.svelte
@@ -19,11 +19,24 @@
       closeModal();
       toast.success('Your username was successfully updated!');
     } catch (error) {
-      if (error?.data?.error === 'Conflict') {
-        toast.danger('The username you selected is already taken. Please choose a diffent one.');
-      } else {
-        toast.danger('There was an error submitting your request. Please try again.');
+      let message;
+      switch (error?.data?.appCode) {
+        case 'USERNAME_RULES_TAKEN':
+          message = 'The username you selected is already taken. Please choose a diffent one.';
+          break;
+        case 'USERNAME_RULES_TOO_SHORT':
+          message = 'Username must have between 3 and 18 characters length.';
+          break;
+        case 'USERNAME_RULES_WEIRD_CHARS':
+          message = 'Username can only contain letters, digits and special characters.';
+          break;
+        case 'USERNAME_RULES_BLANKS':
+          message = 'Username cannot contain blank spaces.';
+          break;
+        default:
+          message = 'There was an error submitting your request. Please try again.';
       }
+      toast.danger(message);
     } finally {
       saving = false;
     }
