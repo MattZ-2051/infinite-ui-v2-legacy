c43c3077cc043d6989ed79882a4dedfa5e2fd51f
Tasos Bekos <tbekos@gmail.com>
2021-08-25 22:44:04 +0300

    feat(lib): support `display` property for `ThemeContext`

--- src/lib/theme/ThemeContext.svelte
+++ src/lib/theme/ThemeContext.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
   import type { Page } from '@sveltejs/kit';
   import { setContext, getContext } from 'svelte';
+  import clsx from 'clsx';
   import { page } from '$app/stores';
+  import { styles } from '$util/styles';
   import { getMatch, getCurrentPage } from './theme.util';
 
   export let id: string;
+  export let display = false;
+
+  let _class = '';
+  /**
+   * Extra classes.
+   */
+  export { _class as class };
 
   const context: string[] = [...((getContext('theme') as string[]) || []), id].filter(Boolean);
   setContext('theme', context);
@@ -12,8 +21,9 @@
   function getThemeClass(_page: Page, _context: string[]) {
     return getMatch([`${getCurrentPage(_page)}`, ..._context].filter(Boolean).join('.'));
   }
+
+  $: style = styles({ display: !display ? 'contents' : '' });
+  $: classes = clsx(getThemeClass($page, context), _class);
 </script>
 
-<div style="display: contents;" class={getThemeClass($page, context)} data-theme={context.join('.')}>
-  <slot />
-</div>
+<div {style} class={classes} data-theme-context={context.join('.')} {...$$restProps}><slot /></div>
