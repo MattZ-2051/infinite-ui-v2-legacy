bc9adac694c20db27640ae9eb316feb6df572732
Tasos Bekos <tbekos@gmail.com>
2021-09-06 15:17:18 +0300

    fix(lib): move tooltip of redeem icon to container

--- src/lib/sku-item/IconRedeem.svelte
+++ src/lib/sku-item/IconRedeem.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import tooltip from '$ui/tooltip';
   import Icon from '$ui/icon/Icon.svelte';
   import iconRedeem from '$lib/components/icons/redeem';
   import iconRedeemed from '$lib/components/icons/redeemed';
@@ -8,12 +9,12 @@
   export let size = 24;
 
   $: iconPath = disabled ? iconRedeemed : iconRedeem;
-  $: tooltip = hasTooltip ? 'Redeemable' : '';
+  $: tooltipMessage = hasTooltip ? 'Redeemable' : '';
   $: padding = size / 5.333_333;
 </script>
 
-<span class="redeem-trigger relative" style="padding: {padding}px"
-  ><Icon path={iconPath} size={`${size - 2 * padding}px`} {tooltip} {...$$restProps} /></span
+<span class="redeem-trigger relative inline-flex" use:tooltip={tooltipMessage} style="padding: {padding}px"
+  ><Icon path={iconPath} size={`${size - 2 * padding}px`} {...$$restProps} /></span
 >
 
 <style lang="postcss">
