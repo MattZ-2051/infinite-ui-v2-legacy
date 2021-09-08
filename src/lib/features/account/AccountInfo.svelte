2cd2ab925ad9c39ff01dc16ab5cf9f82e33bea55
tkotsopoulos <tkotsopoulos@gmail.com>
2021-09-08 13:04:44 +0300

    feat(app): display user email on account settings page

--- src/lib/features/account/AccountInfo.svelte
+++ src/lib/features/account/AccountInfo.svelte
@@ -36,9 +36,7 @@
   <div class="flex flex-col">
     <div class="flex items-center justify-center gap-3 mb-2.5">
       <span class="text-2xl font-bold">@{user.username}</span>
-      <div>
-        <AccountEditButton on:click={() => (editing = true)} />
-      </div>
+      <AccountEditButton on:click={() => (editing = true)} />
     </div>
     {#if user.tagline}<span class="text-white-opacity-70 font-black mb-5 text-center break-words">{user.tagline}</span
       >{/if}
@@ -46,6 +44,11 @@
 {:else}
   <AccountInfoForm on:closeForm={() => (editing = false)} {user} />
 {/if}
-<div class="flex items-center justify-center">
+<div class="flex flex-col gap-2 items-center mt-4 md:mt-24">
+  <div class="flex flex-col gap-2 w-full">
+    <span class="text-sm" style="color:#7D7D7D">Email</span>
+    <span class="font-bold">@{user.email}</span>
+    <div style="height: 1px; background-color: #EBEBEB;" />
+  </div>
   <button type="button" on:click={onResetPassword} class="mt-4 text-white link text-sm ">Reset password</button>
 </div>
