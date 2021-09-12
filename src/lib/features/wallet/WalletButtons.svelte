50fdff6adbe6fb0adfa57415a4fb5c6823f183c8
Joel Del Cueto <wayfaringjou@gmail.com>
2021-09-12 11:13:34 -0400

    fix(app): show toast if user cannot withdraw

--- src/lib/features/wallet/WalletButtons.svelte
+++ src/lib/features/wallet/WalletButtons.svelte
@@ -2,10 +2,19 @@
   import { createEventDispatcher } from 'svelte';
   import { mdiArrowRight } from '@mdi/js';
   import Icon from '$ui/icon/Icon.svelte';
+  import { toast } from '$ui/toast';
 
   const dispatch = createEventDispatcher();
 
   export let canWithdraw: boolean;
+
+  function handleWithdraw() {
+    if (canWithdraw) {
+      dispatch('withdraw');
+    } else {
+      toast.danger('Whoops! You cannot withdraw funds at this time!');
+    }
+  }
 </script>
 
 <div class="flex flex-col divide-y divide-black">
@@ -19,11 +28,8 @@
   </button>
   <button
     type="button"
-    class="flex items-center justify-between py-4 px-10 w-full font-medium text-2xl text-black {canWithdraw
-      ? 'hover:bg-primary hover:text-white'
-      : ''}"
-    on:click={() => dispatch('withdraw')}
-    disabled={!canWithdraw}
+    class="flex items-center justify-between py-4 px-10 w-full font-medium text-2xl text-black hover:bg-primary hover:text-white"
+    on:click={() => handleWithdraw()}
   >
     <span>Withdraw</span>
     <Icon path={mdiArrowRight} size="2" rotate={45} />
