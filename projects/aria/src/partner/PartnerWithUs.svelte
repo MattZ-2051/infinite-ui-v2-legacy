5dcc6eb3b7c2f911b5d061fd6c7f93eec061f109
Joel Del Cueto <wayfaringjou@gmail.com>
2021-09-13 17:51:27 +0300

    fix(app): change partner page for aria based on new requirements

--- projects/aria/src/partner/PartnerWithUs.svelte
+++ projects/aria/src/partner/PartnerWithUs.svelte
@@ -40,33 +40,29 @@
 </script>
 
 <div data-style="container" class="flex flex-col gap-8 text-white bg-black">
-  <div class="flex flex-col gap-14 text-center container mt-8">
+  <div class="flex flex-col gap-12 text-center container mt-8">
     <div class="text-4xl max-w-4xl mx-auto font-medium">
       ARIA Exchange creates exclusive NFTs for the world's most iconic athletes, entertainers, and brands.
     </div>
-    <span class="text-3xl">Partner with us to reach your fans in a whole new way.</span>
+    <div class="mx-auto text-2xl text-center max-w-6xl">
+      We bring you closer to your favorite icons. Our mission is to deliver the most mind blowing and unique digital
+      content in the hottest way possible. We’re obsessed with merging the physical and digital worlds in the most
+      innovative way to give you unlimited access to the most in demand collectibles.
+    </div>
   </div>
 
   <div class="relative flex flex-col">
     <Image src={travisScottBg} class="absolute h-full w-full z-0 object-cover" />
     <div class="z-1 flex flex-col gap-14">
-      <div class="md:py-20  md:mt-16 bg-primary-dark">
-        <div class="container text-2xl text-center py-10 max-w-6xl">
-          We bring you closer to your favorite icons. Our mission is to deliver the most mind blowing and unique digital
-          content in the hottest way possible. We’re obsessed with merging the physical and digital worlds in the most
-          innovative way to give you unlimited access to the most in demand collectibles.
-        </div>
-      </div>
-
-      <div class="container">
+      <div class="container md:mt-16">
         <form use:form class="form flex flex-col gap-8 lg:mx-32 xl:mx-40 px-10 md:mb-16" autocomplete="off">
           <span class="border-b-2 pb-4 pt-7 text-2xl">Talent Details</span>
           <div class="flex justify-between flex-col md:flex-row gap-16">
             <div class="flex flex-col gap-6 text-lg" style="flex-grow:6;">
               <FormInput name="talent" label="Talent Name *" placeholder="John Brown" />
-              <FormInput name="email" label="Email Address *" placeholder="john.brown@example.com" />
-              <FormInput name="expYear" label="Relationship to Talent" placeholder="Self or Other" />
-              <FormInput name="industry" label="Industry" placeholder="Art,Music,Sport etc." />
+              <FormInput name="email" label="Your Email Address *" placeholder="john.brown@example.com" />
+              <FormInput name="expYear" label="Relationship to Talent" placeholder="Self, Manager, etc." />
+              <FormInput name="industry" label="Industry" placeholder="Art, Music, Sport etc." />
             </div>
             <div class="flex flex-col flex-grow gap-1.5 text-lg">
               <span>Social Links</span>
