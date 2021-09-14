3b468cefa2d69d7e0ad6ec7fa8832b30255f01ee
Joel Del Cueto <wayfaringjou@gmail.com>
2021-09-14 12:27:48 -0400

    feat(app): add tooltips to skuedition

--- projects/aria/src/sku-item/SkuEdition.svelte
+++ projects/aria/src/sku-item/SkuEdition.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Sku, Product } from '$lib/sku-item/types';
+  import tooltip from '$ui/tooltip';
   import {
     createSkuMessageType,
     createProductMessageType,
@@ -12,16 +13,29 @@
 
   $: skuSupply = createSkuMessageType(sku);
   $: productSupply = createProductMessageType(product);
+
+  const createTooltipMessage = (supplyType: string | undefined) => {
+    switch (supplyType) {
+      case 'unique':
+        return '1 of 1 available';
+      case 'limited':
+        return 'Pre-determined number of minted collectibles available';
+      case 'released':
+        return 'Number of minted collectibles determined by quantity purchased before listing end date';
+      default:
+        return undefined;
+    }
+  };
 </script>
 
 {#if skuSupply}
-  <div class="flex items-center whitespace-nowrap">
+  <div class="flex items-center whitespace-nowrap" use:tooltip={createTooltipMessage(skuSupply.type)}>
     <div class="{skuSupply.type}-badge w-4 h-4 rounded-full mr-2" />
     <span class="{skuSupply.type}-text">{createSkuMessage(skuSupply.type, skuSupply.quantity, sku)}</span>
   </div>
 {/if}
 {#if productSupply}
-  <div class="flex items-center whitespace-nowrap">
+  <div class="flex items-center whitespace-nowrap" use:tooltip={createTooltipMessage(productSupply.type)}>
     <div class="{productSupply.type}-badge w-4 h-4 rounded-full mr-2" />
     <span class="{productSupply.type}-text"
       >{createProductMessage(productSupply.type, productSupply.quantity, product)}</span
