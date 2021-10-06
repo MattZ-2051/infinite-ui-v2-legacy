2c94c8f83b0ceb7974c2692f90c1439b4fcb36e8
kostas <kchronisdev@gmail.com>
2021-10-06 11:27:50 +0300

    feat(app): allow user to edit avatar image before upload

--- src/lib/features/account/AccountPhotoUpload.svelte
+++ src/lib/features/account/AccountPhotoUpload.svelte
@@ -1,42 +1,19 @@
 <script lang="ts">
-  import { toast } from '$ui/toast';
-  import { post } from '$lib/api';
-  import { updateUser } from '$lib/user';
+  import { openModal } from '$ui/modals';
   import AccountEditButton from './AccountEditButton.svelte';
+  import AccountPhotoUploadModal from './AccountPhotoUploadModal.svelte';
 
   let avatarUpload: HTMLInputElement;
-  let uploading = false;
 
-  async function onFileSelected() {
-    const data = new FormData();
-
-    data.append('image', avatarUpload.files[0]);
-
-    uploading = true;
-    await post('users/photo-upload/me', data)
-      .then(() => updateUser())
-      .catch((error) => {
-        if (error?.data?.appCode === 'PHOTO_TOO_LARGE') {
-          toast.danger('Your image is too large. Please use a profile photo that is < 5 MB.');
-        } else {
-          toast.danger('Failed to upload the avatar image. Please, try again.');
-        }
-      })
-      .finally(() => {
-        uploading = false;
-        avatarUpload.value = '';
-      });
+  function onFileSelected() {
+    let imageSource = URL.createObjectURL(avatarUpload.files[0]);
+    openModal(AccountPhotoUploadModal, { imageSource });
+    avatarUpload.value = '';
   }
 </script>
 
 <div class="absolute bottom-2.5 right-2.5 z-10">
-  <AccountEditButton on:click={() => avatarUpload.click()} loading={uploading} />
+  <AccountEditButton on:click={() => avatarUpload.click()} />
 </div>
 
-<input
-  class="hidden"
-  type="file"
-  accept=".jpg, .jpeg, .png"
-  on:change={async () => await onFileSelected()}
-  bind:this={avatarUpload}
-/>
+<input class="hidden" type="file" accept=".jpg, .jpeg, .png" on:change={onFileSelected} bind:this={avatarUpload} />
---
+++ src/lib/features/account/AccountPhotoUploadModal.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import Cropper from 'cropperjs';
+  import { toast } from '$ui/toast';
+  import { post } from '$lib/api';
+  import { updateUser } from '$lib/user';
+  import { Modal, closeModal } from '$ui/modals';
+  import Button from '$lib/components/Button.svelte';
+  import 'cropperjs/dist/cropper.min.css';
+
+  export let imageSource;
+  let previewImage: HTMLImageElement;
+  let cropper: Cropper;
+  let loading: boolean;
+
+  onMount(() => {
+    cropper = new Cropper(previewImage, {});
+  });
+
+  function cropImage() {
+    const data = new FormData();
+    cropper.getCroppedCanvas().toBlob(function (blob) {
+      data.append('image', blob);
+      uploadImage(data);
+    });
+  }
+
+  async function uploadImage(data) {
+    loading = true;
+    await post('users/photo-upload/me', data)
+      .then(() => updateUser())
+      .catch((error) => {
+        if (error?.data?.appCode === 'PHOTO_TOO_LARGE') {
+          toast.danger('Your image is too large. Please use a profile photo that is < 5 MB.');
+        } else {
+          toast.danger('Failed to upload the avatar image. Please, try again.');
+        }
+      })
+      .finally(() => {
+        loading = false;
+        cropper.destroy();
+        closeModal();
+      });
+  }
+</script>
+
+<Modal title="Edit photo" on:close={closeModal}>
+  <div class="flex flex-col gap-2 mt-4 mb-8 text-base px-10">
+    <div class="w-full mt-4">
+      <img class="h-96" id="image" bind:this={previewImage} src={imageSource} alt="" />
+      <Button
+        disabled={loading}
+        variant="brand"
+        on:click={cropImage}
+        class="self-center w-full py-3 text-2xl text-center mt-6">Save</Button
+      >
+    </div>
+  </div>
+</Modal>
