3c4771628aa52a49b88773c4b0013e54930805f9
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2021-10-20 13:48:08 -0300

    feat(app): develop mobile view for exchange section in about page (#682)
    Closes #95

--- projects/seva/src/about/exchangeTeam/ExchangeTeam.svelte
+++ projects/seva/src/about/exchangeTeam/ExchangeTeam.svelte
@@ -2,33 +2,44 @@
   import { management, advisors } from './Info';
   import TeamGrid from './TeamGrid.svelte';
   import drops from './assets/drops.png';
+  import yellowDropsMobile from './assets/yellowDropsMobile.svg';
   import explosion from './assets/explosion.svg';
+  import explosionMobile from './assets/explosionMobile.svg';
+  import purpleDropsMobile from './assets/purpleDropsMobile.svg';
   import purpleDrops from '../exchangingGoodness/assets/drops.png';
 </script>
 
-<div>
-  <div class="container mt-10 mb-24 lg:mb-0 px-6">
-    <div class="flex flex-col items-center">
-      <div class="font-medium text-4xl sm:text-6xl text-center mb-9 max-w-4xl">The Seva.Love team</div>
-      <div class="text-center max-w-3xl font-medium">Like minded souls united in Love</div>
-    </div>
-
-    <div class=" w-full h-0 flex justify-end">
-      <img src={explosion} alt="star" class="relative h-28 lg:-top-20 hidden lg:block" />
-    </div>
+<div class="container mt-20 mb-24 lg:mb-0 px-6">
+  <div class="flex flex-col items-center">
+    <div class="font-medium text-4xl sm:text-6xl text-center mb-9 max-w-4xl">The Seva.Love team</div>
+    <div class="text-center max-w-3xl font-medium">Like minded souls united in Love</div>
   </div>
 
-  <div class="container">
-    <TeamGrid people={management} title="Management" />
-    <!-- <div style="max-width:var(--container-max-width); padding-left=calc(var(--container-max-width/" -->
-    <div class="w-full h-0 flex justify-end">
-      <img src={purpleDrops} alt="star" class="relative h-24 lg:top-28 hidden 2xl:block 2xl:left-4  " />
-    </div>
+  <div class=" w-full h-0 flex justify-end">
+    <img src={explosion} alt="star" class="relative h-28 lg:-top-20 hidden lg:block" />
   </div>
-  <div class="container">
-    <div class="w-full h-0 flex justify-start">
-      <img src={drops} alt="star" class="relative h-16 lg:-top-80 hidden 2xl:block 2xl:-left-8" />
-    </div>
-    <TeamGrid people={advisors} title="Advisor" />
+</div>
+
+<div class="container">
+  <div class=" w-full h-0 flex justify-end">
+    <img src={explosionMobile} alt="star" class="relative h-28  lg:hidden left-4" />
+  </div>
+  <TeamGrid people={management} title="Management" />
+  <div class="w-full h-0 flex justify-end">
+    <img src={purpleDrops} alt="purple drops" class="relative h-24 lg:top-28 hidden 2xl:block 2xl:left-4  " />
+  </div>
+</div>
+<div class="container">
+  <div class="w-full h-0 flex justify-start">
+    <img src={drops} alt="yellow drops" class="relative h-16 lg:-top-80 hidden 2xl:block 2xl:-left-8" />
+    <img
+      src={yellowDropsMobile}
+      alt="yellow drops"
+      class="relative h-12 sm:h-16 2xl:hidden 2xl:-left-8 right-4 top-12 sm:top-8"
+    />
+  </div>
+  <TeamGrid people={advisors} title="Advisors" />
+  <div class="w-full h-0 flex justify-end pb-24 2xl:hidden">
+    <img src={purpleDropsMobile} alt="purple drops" class="relative h-24 -top-4 left-4  " />
   </div>
 </div>
--- projects/seva/src/about/exchangeTeam/TeamGrid.svelte
+++ projects/seva/src/about/exchangeTeam/TeamGrid.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex flex-col gap-16 md:gap-24 py-16">
   <div class="flex flex-col gap-8 md:gap-12">
-    <div class="flex justify-center"><h1 class="text-4xl font-meidum">{title}</h1></div>
+    <div class="flex justify-center"><h1 class="text-3xl sm:text-4xl font-meidum">{title}</h1></div>
     <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-x-6 md:gap-y-10 max-w-6xl mx-auto">
       {#each people as person}
         <div class="flex flex-col gap-4 items-center text-center">
