69f90d0b526f3f691ffa5312a3856a60de5814dd
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2021-10-20 12:56:44 -0300

    feat(app): wip

--- src/lib/features/card/CardFundSuccess.svelte
+++ src/lib/features/card/CardFundSuccess.svelte
@@ -9,7 +9,7 @@
 </script>
 
 {#if isOpen}
-  <Modal title="Funds added!" on:close={closeModal}>
+  <Modal title="Funds added!">
     <div class="flex flex-col justify-center items-start gap-4 mt-2 text-base max-w-sm py-4 px-10 m-auto">
       <div class="text-base pb-4" style="color: #A8C039">
         <span>Your wallet balance is ${formatCurrency($wallet?.balanceInfo?.totalBalance)} now.</span>
