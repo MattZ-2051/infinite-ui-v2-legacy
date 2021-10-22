1cc8f471841b53486e16dbca757881ee6607fed7
Joel Del Cueto <wayfaringjou@gmail.com>
2021-10-22 10:29:12 -0400

    feat(app): add share button to user owned product

--- src/lib/features/product/Product.svelte
+++ src/lib/features/product/Product.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import { mdiClose } from '@mdi/js';
+  import shareIcon from '$lib/features/sku/assets/share';
   import { browser } from '$app/env';
   import { userId } from '$lib/user';
   import routes from '$project/routes';
   import StickyColumn from '$lib/layout/StickyColumn.svelte';
   import Gallery from '$lib/components/Gallery.svelte';
+  import { socialShareAction } from '$lib/social';
   import Icon from '$ui/icon/Icon.svelte';
   import ProductTabs from './ProductTabs.svelte';
   import ProductInfo from './ProductInfo.svelte';
   import ProductStatus from './status/ProductStatus.svelte';
   import { product } from './product.store';
+  import { isOwner } from './product.service';
 
   export let tab: 'auction' | 'history' | 'owner';
+
+  $: isProductOwner = isOwner($product, $userId);
 
   function onClose() {
     history.back();
@@ -24,18 +29,33 @@
   </div>
   <div class="flex flex-col md:px-0" slot="onscreen-content" style="min-height: calc(100vh - var(--header-height));">
     <div class="mx-4 md:pl-8 mt-8 md:mt-10">
-      <div class="flex items-center text-4xl font-medium justify-between gap-6">
-        <div class="flex items-center">
-          <a sveltekit:prefetch href={routes.sku($product.sku._id)} class="text-gradient-primary">{$product.sku.name}</a
+      <div class="flex md:flex-wrap items-center text-2xl sm:text-3xl md:text-4xl font-medium justify-between gap-6">
+        <div class="flex items-center overflow-hidden">
+          <a
+            sveltekit:prefetch
+            href={routes.sku($product.sku._id)}
+            class="text-gradient-primary truncate md:overflow-visible md:whitespace-pre-wrap">{$product.sku.name}</a
           >
           <span class="mx-3 text-gray-300">/</span>
           <span class="text-gradient-primary">#{$product.serialNumber}</span>
         </div>
-        {#if browser && history.length > 1}
-          <button type="button" on:click={onClose} class="rounded-full bg-white text-black">
-            <Icon path={mdiClose} size="1.33" class="p-1 rounded-full" />
-          </button>
-        {/if}
+        <div class="flex flex-row items-center space-x-4 ml-auto">
+          {#if isProductOwner}
+            <button
+              type="button"
+              class="flex items-center justify-center gap-2 text-center text-base p-2 md:px-5 rounded-full bg-gray-100"
+              use:socialShareAction={$product}
+              ><Icon path={shareIcon} size="1" class="transform scale-90 sm:scale-100 md:scale-110" /><span
+                class="hidden md:inline">Share</span
+              ></button
+            >
+          {/if}
+          {#if browser && history.length > 1}
+            <button type="button" on:click={onClose} class="rounded-full bg-gray-100 text-white">
+              <Icon path={mdiClose} size="1.75" class="transform scale-90 sm:scale-100 md:scale-110 p-1 rounded-full" />
+            </button>
+          {/if}
+        </div>
       </div>
       <div class="mt-6">
         <ProductInfo product={$product} />
