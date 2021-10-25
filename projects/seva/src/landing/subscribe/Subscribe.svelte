b05afffc49b9f8011d96900baaf355bab4702eb1
Fede <federicoalvarez710@gmail.com>
2021-10-25 12:47:04 -0300

    fix(app): show subscribe section even if the user is logged in

--- projects/seva/src/landing/subscribe/Subscribe.svelte
+++ projects/seva/src/landing/subscribe/Subscribe.svelte
@@ -1,10 +1,7 @@
 <script>
-  import { user } from '$lib/user';
   import Subscribe from '../../commonComponents/Subscribe.svelte';
 </script>
 
-{#if !$user}
-  <div class="container flex justify-center px-6">
-    <Subscribe />
-  </div>
-{/if}
+<div class="container flex justify-center px-6">
+  <Subscribe />
+</div>
