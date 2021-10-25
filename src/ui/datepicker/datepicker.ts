a9cec0669d448de9be562d331ace804362be87c2
tkotsopoulos <tkotsopoulos@gmail.com>
2021-10-25 15:46:33 +0300

    feat(ui): add lazy load for datepicker

--- src/ui/datepicker/datepicker.ts
+++ src/ui/datepicker/datepicker.ts
@@ -1,18 +1,19 @@
 import type { BaseOptions } from 'flatpickr/dist/types/options';
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.css';
 
 export function datePicker(node, config): SvelteActionReturn {
-  const instance = flatpickr(node, config);
+  (async () => {
+    const [{ default: flatpickr }] = await Promise.all([import('flatpickr'), import('flatpickr/dist/flatpickr.css')]);
+    const instance = flatpickr(node, config);
 
-  return {
-    update(newConfig: BaseOptions) {
-      for (const item in newConfig) {
-        instance.set(item as keyof BaseOptions, newConfig[item]);
-      }
-    },
-    destroy() {
-      instance.destroy();
-    },
-  };
+    return {
+      update(newConfig: BaseOptions) {
+        for (const item in newConfig) {
+          instance.set(item as keyof BaseOptions, newConfig[item]);
+        }
+      },
+      destroy() {
+        instance.destroy();
+      },
+    };
+  })();
 }
