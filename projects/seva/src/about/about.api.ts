2e422cb15dc4547aaa8fc55dac540549bf6e47bc
Joel Del Cueto <wayfaringjou@gmail.com>
2021-11-03 10:42:51 -0400

    fix(app): update sku id for seva landing

--- projects/seva/src/about/about.api.ts
+++ projects/seva/src/about/about.api.ts
@@ -2,7 +2,7 @@ import type { Sku } from '$lib/sku-item/types';
 import { get } from '$lib/api';
 
 export async function loadSingleSku({ fetch }: { fetch: Fetch }) {
-  const singleSkuData = await get<Sku>(`skus/615640e1e5b85e4affdb2316/?includeFunctions=true`, { fetch });
+  const singleSkuData = await get<Sku>(`skus/6181759e0318051e2ac2c2df/?includeFunctions=true`, { fetch });
 
   return { singleSkuData };
 }
