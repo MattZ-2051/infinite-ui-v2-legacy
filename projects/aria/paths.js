5928daac305e3ae2ade3a090943a927025576808
Alejandro Alvarez <alealvarez14@gmail.com>
2021-11-16 20:23:04 +0000

    fix(app): add TOS page to Aria (#812)
    Co-authored-by: Alejandro Alvarez <alejandroalvarez@Alejandros-MacBook-Pro.local>

--- projects/aria/paths.js
+++ projects/aria/paths.js
@@ -1,3 +1,4 @@
 export const paths = {
   '$project/header/*': 'projects/infinite/src/header/*',
+  '$project/TermsConditions.svelte': 'projects/infinite/src/TermsConditions.svelte',
 };
--- projects/aria/src/TermsConditions.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { Seo } from '$lib/seo';
-</script>
-
-<Seo title="Terms and conditions" />
-
-<div class="container py-8">
-  <h1 class="text-center text-3xl my-4 "><strong>Terms of Service</strong></h1>
-</div>
