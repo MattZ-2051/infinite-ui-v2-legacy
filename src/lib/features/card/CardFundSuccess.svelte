b8989d3192c592fca3c4437d1edab7ec867675e8
Fede <federicoalvarez710@gmail.com>
2021-11-17 09:27:56 -0300

    fix(app): remove the text 'now' from the success funds to modal
    closes 1028

--- src/lib/features/card/CardFundSuccess.svelte
+++ src/lib/features/card/CardFundSuccess.svelte
@@ -13,7 +13,7 @@
     <div slot="title"><span class="text-3xl pr-8 font-semibold">Success!</span></div>
     <div class="flex flex-col justify-center items-start gap-4 mt-2 text-base max-w-sm py-4 px-10 m-auto">
       <div class="text-base pb-4" style="color: #A8C039">
-        <span>Your new wallet balance is {formatCurrency($wallet?.balanceInfo[0]?.totalBalance)} now.</span>
+        <span>Your new wallet balance is {formatCurrency($wallet?.balanceInfo[0]?.totalBalance)}.</span>
       </div>
       <div class="text-gray-300 text-base">Your funds have officially been added.</div>
       <div class="flex flex-col gap-4 w-full pb-2 pt-4">
