2e94f98ffac19c62e218286b2561d46f9b0178e4
gtorterolo-zircon <69865250+gtorterolo-zircon@users.noreply.github.com>
2021-11-22 16:24:18 -0300

    fix(app): minor text changes (#862)

--- src/lib/features/wallet/wallet.service.ts
+++ src/lib/features/wallet/wallet.service.ts
@@ -18,9 +18,9 @@ export function handleWalletDepositError(error: ApiError) {
     case 'ABOVE_TOTAL_DEPOSIT_LIMIT':
       return `Whoops! You've deposited ${formatCurrency(
         error.data.args['totalSelectedCardDepositCents'] / 100
-      )}  USD in the past 24 hours. The current allowable daily limit is ${formatCurrency(
+      )}  USD. The current allowable lifetime limit is ${formatCurrency(
         error.data.args['totalDepositCentsLimit'] / 100
-      )}  USD. Please try again when 24 hours have passed or deposit cryptocurrency.`;
+      )}  USD. If you would like to deposit more funds, you may do so using cryptocurrency."`;
     default:
       return 'We couldn’t process your payment and the transaction was cancelled.';
   }
