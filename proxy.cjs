2912e0cac76d5281cb175cd6b6d30b5c1dff9b0f
Tasos Bekos <tbekos@gmail.com>
2021-11-26 16:49:22 +0200

    fix(repo): make api url for local dev envs work through proxy

--- proxy.cjs
+++ proxy.cjs
@@ -1,12 +1,9 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
-const { _project: project } = require('./tsconfig.json');
-require('dotenv').config({ path: `./projects/${project}/.env.development` });
 
 const proxy = httpProxy.createProxyServer({});
 http
   .createServer(function (req, res) {
-    // console.log('Request', req.method, req.url);
-    proxy.web(req, res, { target: process.env.VITE_API_URL, changeOrigin: true });
+    proxy.web(req, res, { target: 'https://api-dev.infiniteworld.com', changeOrigin: true });
   })
   .listen(3001);
