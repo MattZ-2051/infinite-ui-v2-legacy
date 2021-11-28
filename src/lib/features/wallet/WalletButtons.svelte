efd7807bdc97582fc6d51073d25506b88619e6ec
Lucas H <lhenning@citizensreserve.com>
2021-11-28 20:25:19 +0100

    fix(app): deposit button enable (#901)

--- src/lib/features/wallet/WalletButtons.svelte
+++ src/lib/features/wallet/WalletButtons.svelte
@@ -2,27 +2,22 @@
   import { createEventDispatcher } from 'svelte';
   import arrowRight from '$lib/features/wallet/assets/arrow-right';
   import Icon from '$ui/icon/Icon.svelte';
-  import tooltip from '$ui/tooltip';
-
   const dispatch = createEventDispatcher();
 </script>
 
 <div class="flex flex-col divide-y divide-black border-t md-border-t-0 md:border-r border-black">
-  <div use:tooltip={'Deposits will be enabled before drops go live.'}>
-    <button
-      type="button"
-      class="sticky-cta group flex items-center justify-between py-4 px-10 w-full font-medium text-2xl opacity-50"
-      disabled={true}
-      on:click={() => dispatch('deposit')}
-    >
-      <span>Deposit</span>
-      <Icon
-        class="transform transition-transform duration-150 -rotate-45 group-hover:-rotate-90"
-        path={arrowRight}
-        size="2"
-      />
-    </button>
-  </div>
+  <button
+    type="button"
+    class="sticky-cta group flex items-center justify-between py-4 px-10 w-full font-medium text-2xl"
+    on:click={() => dispatch('deposit')}
+  >
+    <span>Deposit</span>
+    <Icon
+      class="transform transition-transform duration-150 -rotate-45 group-hover:-rotate-90"
+      path={arrowRight}
+      size="2"
+    />
+  </button>
   <button
     type="button"
     class="sticky-cta group flex items-center justify-between py-4 px-10 w-full font-medium text-2xl"
