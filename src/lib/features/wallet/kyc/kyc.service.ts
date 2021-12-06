269076d163015a341e96a27ffdc0ccccf1173578
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2021-12-06 14:06:43 -0300

    fix(app): fix message on kyc service to erase extra quote
    Closes #243

--- src/lib/features/wallet/kyc/kyc.service.ts
+++ src/lib/features/wallet/kyc/kyc.service.ts
@@ -14,7 +14,7 @@ export function getDailyDepositLimitDisclaimer(level: number, dailyDepositLimit:
 export const getKYCLevelDepositDisclaimer = (level: number) => {
   switch (level) {
     case 0:
-      return `'You may deposit funds using credit card through our partner MoonPay. You may check MoonPay limits <a target="_blank" rel="noopener noreferrer" class="underline" href="https://support.moonpay.com/hc/en-gb/articles/4407710995217-What-are-your-account-limits-">here</a>.<br/>
+      return `You may deposit funds using credit card through our partner MoonPay. You may check MoonPay limits <a target="_blank" rel="noopener noreferrer" class="underline" href="https://support.moonpay.com/hc/en-gb/articles/4407710995217-What-are-your-account-limits-">here</a>.<br/>
         Level 1 verification is required for cryptocurrency deposits under $50,000 USD and Level 2 verification is required for cryptocurrency deposits equal to or over $50,000 USD.`;
     case 1:
       return `You may deposit funds using credit card through our partner MoonPay. You may check MoonPay limits <a target="_blank" rel="noopener noreferrer" class="underline" href="https://support.moonpay.com/hc/en-gb/articles/4407710995217-What-are-your-account-limits-">here</a>.<br/>
