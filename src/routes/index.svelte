7f97f6b6c37568e53de200cbb3c0d96c8739ddf7
Tasos Bekos <tbekos@gmail.com>
2021-12-17 19:45:52 +0200

    feat(app): support redirecting index route to marketplace

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,18 @@
 <script context="module" lang="ts">
   import type { Awaited } from 'ts-essentials';
+  import routes from '$project/routes';
   import { loadData } from '$project/landing/landing.api';
 
+  const redirect = import.meta.env.VITE_REDIRECT_INDEX;
+
   export async function load({ fetch }) {
+    if (redirect) {
+      return {
+        status: 302,
+        redirect: routes.marketplace,
+      };
+    }
+
     return {
       props: { data: await loadData({ fetch }) },
     };
