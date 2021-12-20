1ba80b466126137a081ff84a8dab2a1d06209f07
Alejandro Alvarez <alealvarez14@gmail.com>
2021-12-20 19:58:33 -0300

    fix(app): tiktok link on infinite
    Closes PI-166

--- projects/infinite/src/footer/social-links.ts
+++ projects/infinite/src/footer/social-links.ts
@@ -5,5 +5,5 @@ export const links = [
   { id: 'Facebook', href: 'https://www.facebook.com/infinitebysuku', path: mdiFacebook },
   { id: 'Twitter', href: 'https://twitter.com/infinitebysuku', path: mdiTwitter },
   { id: 'Instagram', href: 'https://www.instagram.com/infinitebysuku/', path: mdiInstagram },
-  { id: 'TikTok', href: 'https://www.tiktok.com/@infinitebysuku?lang=en', path: siTiktok.path },
+  { id: 'TikTok', href: 'https://www.tiktok.com/@_infiniteworld?lang=en', path: siTiktok.path },
 ];
