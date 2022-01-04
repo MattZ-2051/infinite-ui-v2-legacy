9c936670cc3d44f0670f47b8255e79ad022f81a6
Fede <federicoalvarez710@gmail.com>
2022-01-04 17:02:54 -0300

    feat(app): change header logo on aria
    closes #1099

--- projects/aria/src/Logo.svelte
+++ projects/aria/src/Logo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import logo from '$project/assets/logo-aria-white.png?h=85&format=avif;webp;png&metadata=src;format;width;height';
+  import logo from '$project/assets/aria_logo_alt_white.png?h=40&format=avif;webp;png&metadata=src;format;width;height';
   import Image from '$ui/image/Image.svelte';
 </script>
 
