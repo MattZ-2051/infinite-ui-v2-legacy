1901c581654955c92669ceef3d44e41e981a02b9
Fede <federicoalvarez710@gmail.com>
2022-01-14 12:36:48 -0300

    fix(app): handle error for unverified emails in  auth0

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -25,24 +25,28 @@
 
 <ThemeContext id="error" class="flex flex-grow items-center justify-center" display>
   <div class="container flex flex-col gap-5 md:gap-10 items-center text-center">
-    <div class="text-6xl font-extrabold text-gradient-primary">Oh, no!</div>
-    <div class="flex flex-col gap-4 md:gap-8">
-      {#if status === 404}
-        <div class="text-2xl md:text-5xl font-semibold">This page does not exist.</div>
-      {:else}
-        <div class="text-2xl md:text-5xl font-semibold">There was an unexpected problem.</div>
-        <div class="text-sm md:text-base text-gray-500">
-          Please try again in a few minutes or <a href={routes.help} class="hover:underline ">contact support.</a>
-        </div>
-      {/if}
-    </div>
-    <Button variant="brand" href={routes.index}>Go to homepage</Button>
-    {#if dev}
-      {#if error.frame}
-        <pre class="m-4 p-4 border whitespace-pre-line">{error.frame}</pre>
-      {/if}
-      {#if error.stack}
-        <pre class="m-4 p-4 border whitespace-pre-line">{error.stack}</pre>
+    {#if error.message === 'Please verify your email before logging in.'}
+      <div class="text-xl md:text-3xl font-semibold">{error.message}</div>
+    {:else}
+      <div class="text-6xl font-extrabold text-gradient-primary">Oh, no!</div>
+      <div class="flex flex-col gap-4 md:gap-8">
+        {#if status === 404}
+          <div class="text-2xl md:text-5xl font-semibold">This page does not exist.</div>
+        {:else}
+          <div class="text-2xl md:text-5xl font-semibold">There was an unexpected problem.</div>
+          <div class="text-sm md:text-base text-gray-500">
+            Please try again in a few minutes or <a href={routes.help} class="hover:underline ">contact support.</a>
+          </div>
+        {/if}
+      </div>
+      <Button variant="brand" href={routes.index}>Go to homepage</Button>
+      {#if dev}
+        {#if error.frame}
+          <pre class="m-4 p-4 border whitespace-pre-line">{error.frame}</pre>
+        {/if}
+        {#if error.stack}
+          <pre class="m-4 p-4 border whitespace-pre-line">{error.stack}</pre>
+        {/if}
       {/if}
     {/if}
   </div>
