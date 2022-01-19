d2b579b26fb795c6880fb5645b42381adabd9eff
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2022-01-19 15:51:10 -0300

    feat(app): add verified email page (#1105)
    Closes #SM-86

---
+++ src/routes/auth/email-verified/index.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+  import ThemeContext from '$lib/theme/ThemeContext.svelte';
+  import Button from '$lib/components/Button.svelte';
+  import routes from '$project/routes';
+  import { user, onSignIn } from '$lib/user';
+</script>
+
+<ThemeContext id="error" class="flex flex-grow items-center justify-center" display>
+  <div class="container flex flex-col gap-5 md:gap-10 items-center text-center">
+    {#if $user}
+      <div class="text-xl md:text-3xl font-semibold">Your email was successfully verified and you are logged in!</div>
+      <div><Button variant="brand" href={routes.marketplace}>Continue to marketplace!</Button></div>
+    {:else}
+      <div class="text-xl md:text-3xl font-semibold">Your email was successfully verified</div>
+      <div><Button variant="brand" on:click={onSignIn}>Sign in!</Button></div>
+    {/if}
+  </div>
+</ThemeContext>
