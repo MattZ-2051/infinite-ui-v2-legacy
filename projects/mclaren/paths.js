2648bbc5956e9bdfc936cea4683bc50da568f1ee
Fede <federicoalvarez710@gmail.com>
2022-02-02 13:20:20 -0300

    fix(app): customizing the wallet
    closes #ci71.2

--- projects/mclaren/paths.js
+++ projects/mclaren/paths.js
@@ -2,15 +2,27 @@ export const paths = {
   '$project/about/*': 'projects/aria/src/about/*',
   '$project/collection/*': 'projects/aria/src/collection/*',
   '$ui/pagination/assets/chevron-left': 'projects/infinite/src/assets/lib/arrow-left',
+<<<<<<< HEAD
   '$lib/features/wallet/assets/arrow-right': 'projects/mclaren/src/assets/lib/arrow-right',
   '$lib/features/wallet/assets/info': 'projects/mclaren/src/assets/lib/info',
+=======
+  '$lib/features/wallet/assets/arrow-right': 'projects/mcLaren/src/assets/lib/arrow-right',
+  '$lib/features/wallet/assets/info': 'projects/mcLaren/src/assets/lib/info',
+>>>>>>> af7297f2 (fix(app): customizing the wallet)
   '$lib/features/sku/assets/arrow-right': 'projects/infinite/src/assets/lib/arrow-right',
   '$lib/features/sku/assets/share': 'projects/infinite/src/assets/lib/share',
   '$lib/components/icons/magnify': 'projects/infinite/src/assets/lib/magnify',
   '$lib/features/account/Account.svelte': 'projects/mclaren/src/account/Account.svelte',
   '$lib/features/account/assets/PencilIcon.svelte': 'projects/mclaren/src/assets/lib/PencilIcon.svelte',
+<<<<<<< HEAD
   '$lib/features/product/assets/arrow-right': 'projects/mclaren/src/assets/lib/arrow-right',
   '$lib/features/wallet/kyc/assets/ArrowRight.svelte': 'projects/mclaren/src/assets/lib/arrow-right.svelte',
   '$lib/components/icons/arrow-down': 'projects/mclaren/src/assets/lib/arrow-down',
   '$lib/components/icons/arrow-up': 'projects/mclaren/src/assets/lib/arrow-up',
+=======
+  '$lib/features/product/assets/arrow-right': 'projects/mcLaren/src/assets/lib/arrow-right',
+  '$lib/features/wallet/kyc/assets/ArrowRight.svelte': 'projects/mcLaren/src/assets/lib/arrow-right.svelte',
+  '$lib/components/icons/arrow-down': 'projects/mcLaren/src/assets/lib/arrow-down',
+  '$lib/components/icons/arrow-up': 'projects/mcLaren/src/assets/lib/arrow-up',
+>>>>>>> af7297f2 (fix(app): customizing the wallet)
 };
