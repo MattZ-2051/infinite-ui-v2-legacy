2057bad22bebe9b4bc7bcd593d1db07034bfae47
Matt Zamora <mzamora2899@gmail.com>
2022-02-08 17:30:27 -0700

    feat(app): add mclaren feature collection comp

--- projects/mclaren/src/landing/Landing.svelte
+++ projects/mclaren/src/landing/Landing.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import Hero from './Hero.svelte';
   import FAQ from './faq/FAQ.svelte';
+  import FeaturedCollection from './FeaturedCollection.svelte';
 
   export const skus = [];
 </script>
 
 <div>
   <Hero />
+  <FeaturedCollection />
   <FAQ />
 </div>
 
