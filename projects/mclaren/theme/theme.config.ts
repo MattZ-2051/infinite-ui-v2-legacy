15765da928f3618d42966ff80084742727198532
Fede <federicoalvarez710@gmail.com>
2022-02-10 13:54:32 -0300

    fix(app): fix wallet section
    closes ci-321

--- projects/mclaren/theme/theme.config.ts
+++ projects/mclaren/theme/theme.config.ts
@@ -2,19 +2,22 @@ const Theme = {
   Dark: 'theme-dark',
   Light: 'theme-light',
   variant2: 'theme-variant-2',
+  WalletBalance: 'theme-wallet-balance',
+  WalletButtons: 'theme-brand-wallet-buttons',
   // BrandFooter: 'theme-brand-footer',
 };
 
 export const themeConfig = {
   '*.header': Theme.variant2,
   '$index.root': Theme.variant2,
+  '*.wallet-buttons': Theme.WalletButtons,
+  '*.wallet-balance': Theme.WalletBalance,
   // '$marketplace.root': Theme.Light,
   // '$marketplace.*.footer': Theme.BrandFooter,
   // '$marketplace.root.footer': Theme.Light,
   // '$u.$wallet.$deposit.root': Theme.Light,
   // '$help.*.root': Theme.Light,
   // '*.sku-offsreen': Theme.Light,
-  // '*.wallet-list': Theme.Light,
   // '*.modal': Theme.Light,
   // '*.error': Theme.Light,
 };
