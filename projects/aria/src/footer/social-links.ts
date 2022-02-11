c7a7714fa04ece4dd105bde22f799985282eb13f
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2022-02-11 15:18:16 -0300

    fix(app): remove social icons from aria footer

--- projects/aria/src/footer/social-links.ts
+++ projects/aria/src/footer/social-links.ts
@@ -1,29 +1,7 @@
-import { mdiTwitter, mdiInstagram, mdiDiscord, mdiFacebook, mdiLinkedin, mdiPinterest, mdiReddit } from '@mdi/js';
-import { siSnapchat } from 'simple-icons/icons';
+import { mdiTwitter, mdiInstagram, mdiDiscord } from '@mdi/js';
 
 export const links = [
   { id: 'Instagram', href: 'https://www.instagram.com/theariaexchange/', path: mdiInstagram },
   { id: 'Twitter', href: 'https://twitter.com/TheAriaExchange', path: mdiTwitter },
-  {
-    id: 'Facebook',
-    href: 'https://www.facebook.com/The-ARIA-Exchange-101775858801456',
-    path: mdiFacebook,
-  },
-  { id: 'LinkedIn', href: 'https://www.linkedin.com/company/the-aria-exchange/', path: mdiLinkedin },
-  {
-    id: 'Pinterest',
-    href: 'https://www.pinterest.com/TheARIAExchange',
-    path: mdiPinterest,
-  },
-  {
-    id: 'Reddit',
-    href: 'https://www.reddit.com/user/TheARIAExchange',
-    path: mdiReddit,
-  },
   { id: 'Discord', href: 'https://discord.com/invite/3JtgwbduBt', path: mdiDiscord },
-  {
-    id: 'Snapchat',
-    href: 'https://www.snapchat.com/add/theariaexchange?share_id=MzQ0RkMz&locale=en_US',
-    path: siSnapchat.path,
-  },
 ];
