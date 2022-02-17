17e16ee544aba9e8834436653db60892760b613e
Matt Zamora <mzamora2899@gmail.com>
2022-02-17 14:23:57 -0700

    feat(app): add mclaren philosophy mobile styles

--- projects/mclaren/src/landing/McLarenPhilosophy.svelte
+++ projects/mclaren/src/landing/McLarenPhilosophy.svelte
@@ -5,18 +5,24 @@
 
 <div class="relative">
   <div class="flex justify-center items-center h-screen">
-    <div class="text-white w-full z-10 flex-col max-w-7xl items-center justify-center">
-      <p class="text-xl font-normal pb-2 uppercase" style="letter-spacing:0.2em;">nft, it's as easy as 1-2-3</p>
-      <p class="tracking-widest text-5xl font-light pb-8">McLAREN PHILOSOPHY</p>
-      <p class="text-xl font-light max-w-lg pb-6 leading-9">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit urna nulla, in pretium orci
-        sollicitudin sed. Nunc sed purus et nunc sollicitudin.
+    <div class="text-white w-full z-10 flex-col max-w-7xl items-center justify-center container">
+      <p class="text-base sm:text-xl font-normal pb-2 uppercase second-font" style="letter-spacing:0.2em;">
+        nft, it's as easy as 1-2-3
       </p>
-      <p class="text-xl font-light max-w-lg leading-9 pb-12">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit urna nulla, in pretium orci
-        sollicitudin sed. Nunc sed purus et nunc sollicitudin.
+      <p class="tracking-widest sm:text-5xl text-3xl font-light pb-8 second-font leading-10 sm:leading-normal">
+        McLAREN PHILOSOPHY
       </p>
-      <Button variant="brand" class="h-16 w-80 font-bold text-xs tracking-widest uppercase"
+      <div class="text-base sm:text-xl font-light max-w-lg leading-9">
+        <p class="pb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit urna nulla, in pretium orci
+          sollicitudin sed. Nunc sed purus et nunc sollicitudin.
+        </p>
+        <p class="pb-12">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit urna nulla, in pretium orci
+          sollicitudin sed. Nunc sed purus et nunc sollicitudin.
+        </p>
+      </div>
+      <Button variant="brand" class="h-12 sm:h-16 w-full sm:w-80 font-bold text-xs tracking-widest uppercase"
         >discover our philosophy</Button
       >
     </div>
