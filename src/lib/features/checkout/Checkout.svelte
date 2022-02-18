323afc85c95abb131251c1fe76f04a7e5355f179
Diego Pizzarello <diegopizzarello@gmail.com>
2022-02-18 14:58:31 -0300

    feat(app): add exit checkout component

--- src/lib/features/checkout/Checkout.svelte
+++ src/lib/features/checkout/Checkout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import type { Product, Sku } from '$lib/sku-item/types';
   import { getActiveListings } from '$lib/features/sku/sku.service';
+  import { goto } from '$app/navigation';
+  import routes from '$project/routes';
   import OrderSummary from './OrderSummary.svelte';
+  import ExitCheckout from './ExitCheckout.svelte';
 
   export let sku: Sku = undefined;
   export let product: Product = undefined;
@@ -9,8 +12,13 @@
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const _sku = sku ? sku : product.sku;
   const [listing] = getActiveListings(sku);
+
+  let exitCheckout = false;
 </script>
 
 <div class="container grid grid-cols-2">
   <OrderSummary {sku} {product} {listing} />
+  {#if exitCheckout}
+    <ExitCheckout onReturn={() => (exitCheckout = false)} onExit={() => goto(routes.sku(_sku._id))} />
+  {/if}
 </div>
---
+++ src/lib/features/checkout/ExitCheckout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte';
+
+  export let onReturn: () => void;
+  export let onExit: () => void;
+</script>
+
+<div class="flex flex-col max-w-sm justify-center justify-self-center	">
+  <span class="text-3xl mb-10">Exit checkout?</span>
+  <span class="mb-10">If you exit before the payment process is completed, any progress you made will be lost.</span>
+  <Button variant="brand" on:click={onReturn}>Return to checkout</Button>
+  <span class="mt-6 text-center text-gray-500 hover:underline cursor-pointer hover:text-gray-800" on:click={onExit}
+    >Exit and back to Marketplace</span
+  >
+</div>
