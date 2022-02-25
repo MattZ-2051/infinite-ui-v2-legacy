329d63099c26b9a603614af0522406af471890d0
gnieves-zircontech <85505913+gnieves-zircontech@users.noreply.github.com>
2022-02-25 16:44:44 -0300

    fix(app): user collection mclaren mobile fixes
    closes ci469

--- projects/mclaren/src/collection/CollectionUser.svelte
+++ projects/mclaren/src/collection/CollectionUser.svelte
@@ -12,22 +12,24 @@
   {#if profile.profilePhotoUrl}
     <img src={`${profile.profilePhotoUrl}?t=${profile.updatedAt}`} alt="" class="w-24 h-24 p-1 rounded-full" />
   {/if}
-  <div class="text-xl md:text-2xl font-medium text-center mt-6 title-font">
+  <div class="text-3xl font-medium text-center mt-6 title-font">
     @{profile.username}
   </div>
   {#if profile.tagline}
-    <div class="mt-2 text-sm font-medium text-gray-500 text-center">{profile.tagline}</div>
+    <div class="mt-2 text-xs sm:text-sm font-medium sm:text-gray-500 text-center">{profile.tagline}</div>
   {/if}
   {#if own}
-    <ButtonGroup class="grid grid-flow-col auto-cols-auto overflow-hidden mt-6 gap-x-7">
+    <ButtonGroup
+      class="grid grid-flow-row gap-y-2 md:grid-flow-col auto-cols-auto overflow-hidden mt-6 gap-x-7 w-full sm:w-auto "
+    >
       <Button
         href={routes.wallet}
-        class="flex items-center justify-center text-center py-3 px-5 w-60"
+        class="flex items-center justify-center text-center py-3 px-5  mx-2 sm:mx-0 sm:w-60"
         variant="outline-brand">My Wallet</Button
       >
       <Button
         href={routes.account}
-        class="flex items-center justify-center text-center py-3 px-5 w-60"
+        class="flex items-center justify-center text-center py-3 px-5 mx-2 sm:mx-0 sm:w-60"
         variant="outline-brand">Account Settings</Button
       >
     </ButtonGroup>
