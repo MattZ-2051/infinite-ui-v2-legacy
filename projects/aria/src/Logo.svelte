fa20b3cd2667fa1abe58445265b0fe27f3671909
Joel Del Cueto <wayfaringjou@gmail.com>
2022-03-10 21:32:31 +0100

    fix(app): include beta in aria logos
    Closes SM-159

--- projects/aria/src/Logo.svelte
+++ projects/aria/src/Logo.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import logo from '$project/assets/aria_logo_alt_white.png?h=40&format=avif;webp;png&metadata=src;format;width;height';
-  import Image from '$ui/image/Image.svelte';
+  import Logo from '$project/assets/aria_logo_alt_white.svelte';
 </script>
 
-<Image src={logo} />
+<Logo class="h-10 w-auto max-w-[340px] lg:max-w-full" />
