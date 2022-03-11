b9b26bc51646f3923f0e5ec82143b556ea1f5f11
Matt Zamora <mzamora2899@gmail.com>
2022-03-10 17:50:33 -0700

    test(app): add storybooks for voucher modal

---
+++ src/lib/features/sku/voucher/VoucherModal.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import VoucherModal from './VoucherModal.svelte';
+</script>
+
+<Meta
+  title="Lib / features / sku / Voucher Modal"
+  component={VoucherModal}
+  argTypes={{
+    isValidCode: {
+      options: [undefined, true, false],
+      control: { type: 'select' },
+    },
+    voucherCode: {
+      control: { type: 'text' },
+    },
+  }}
+/>
+
+<Template let:args>
+  <div class="max-w-5xl">
+    <VoucherModal {...args} />
+  </div>
+</Template>
+
+<Story name="Modal" args={{ isValidCode: undefined, voucherCode: '' }} />
