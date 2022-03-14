2998f3a850871007df267edde5e8ce7d3ae9398d
Joel Del Cueto <wayfaringjou@gmail.com>
2022-03-14 15:40:19 +0100

    fix(app): change homepage route in aria to external landing
    For SM-159

--- projects/aria/src/routes.ts
+++ projects/aria/src/routes.ts
@@ -3,6 +3,7 @@ import routes from '$lib/routes';
 
 export default {
   ...routes,
+  index: process.env.NODE_ENV === 'production' ? 'https://www.ariaexchange.com' : 'https://uat.ariaexchange.com',
   help: 'https://aria-network.force.com/support/s/',
   careers: 'https://jobs.wrkhq.com/the-aria-network',
 };
