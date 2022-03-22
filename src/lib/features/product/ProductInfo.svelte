f131d0094caaa7af86ff6ef810f78d3c442325ab
Matt Zamora <mzamora2899@gmail.com>
2022-03-22 14:35:53 -0600

    fix(lib): show text only for mint later nft

--- src/lib/features/product/ProductInfo.svelte
+++ src/lib/features/product/ProductInfo.svelte
@@ -134,7 +134,7 @@
   <div class={cellClass}>
     {#if sku.currency === 'ETH'}
       <div class={headerClass}>ERC721 Transaction</div>
-      {#if mintStatus !== 'processed'}
+      {#if mintStatus !== 'processed' && sku?.mintPolicy?.transaction === 'later'}
         <span>Not Minted</span>
       {:else}
         <div class="flex flex-row items-center">
