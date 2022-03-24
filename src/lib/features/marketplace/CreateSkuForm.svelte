48980ec2b9e6dab42689e80a8489262441221cd6
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2022-03-24 21:51:59 +0200

    feat(lib): improve sku form layout

--- src/lib/features/marketplace/CreateSkuForm.svelte
+++ src/lib/features/marketplace/CreateSkuForm.svelte
@@ -6,6 +6,7 @@
   import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
   import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
   import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+  import { slide } from 'svelte/transition';
   import { setContext } from 'svelte';
   import type { CurrencyType } from '../wallet/types';
   import type { FileAsset } from '$ui/file/types';
@@ -16,7 +17,7 @@
   import { openModal } from '$ui/modals';
   import ConfirmModal from '$lib/components/ConfirmModal.svelte';
   import { browser } from '$app/env';
-  import { FormElement } from '$lib/components/form';
+  import { FormElement, Textarea } from '$lib/components/form';
   import Icon from '$ui/icon/Icon.svelte';
   import Button from '$lib/components/Button.svelte';
   import DualRingLoader from '$lib/components/DualRingLoader.svelte';
@@ -25,6 +26,7 @@
   import { mdiArrowLeft } from '@mdi/js';
   import { goto } from '$app/navigation';
   import routes from '$project/routes';
+  import Switch from '$lib/components/Switch.svelte';
 
   registerPlugin(FilePondPluginFileValidateType, FilePondPluginFileValidateSize, FilePondPluginImagePreview);
 
@@ -36,10 +38,6 @@
       .string()
       .required('Full name is required.')
       .max(NAME_MAX_CHARS, `Full name must be shorter than ${NAME_MAX_CHARS} characters.`),
-    description: yup
-      .string()
-      .required('Description is required.')
-      .max(DESCRIPTION_MAX_CHARS, `About me must be at most ${DESCRIPTION_MAX_CHARS} characters.`),
     royaltyFeePercentage: yup
       .number()
       .min(0, 'Royalties must be greater than or equal to 0.')
@@ -86,6 +84,8 @@
     validate: validateSchema(schema),
   });
 
+  let hasUnlockableContent = false;
+
   function onClose() {
     history.back();
   }
@@ -122,7 +122,7 @@
     {/if}
 
     <div class="flex flex-col gap-3">
-      <div class="text-2xl">Upload an image, Video or Audio</div>
+      <div class="text-2xl">Upload an image</div>
       <div class="text-sm	text-gray-500 flex justify-between">
         <div>Format: JPG, PNG or GIF. Max size: 10MB.</div>
         <div>*Mandatory</div>
@@ -135,11 +135,15 @@
       maxFileSize="10MB"
       credits={false}
     />
+
+    <div class="text-2xl">Item Details</div>
     <FormElement label="Full name*" name="name" helperText={`${$data.name.length} / ${NAME_MAX_CHARS}`} />
     <FormElement
-      label="Description*"
+      label="Description"
       name="description"
+      component={Textarea}
       helperText={`${$data.description.length} / ${DESCRIPTION_MAX_CHARS}`}
+      rows={3}
     />
     <FormElement
       type="number"
@@ -149,6 +153,31 @@
       after="%"
     />
     <FormElement label="Supply" name="maxSupply" disabled helperText="Amount of copies that can be minted." />
+
+    <div class="flex flex-col gap-4">
+      <div class="flex justify-between items-center">
+        <div class="text-2xl">Unlockable Content</div>
+        <Switch bind:checked={hasUnlockableContent} />
+      </div>
+      <div class="text-sm	text-gray-500 flex justify-between">
+        <div>Add unlockable content that can only be revealed by the owner of the item.</div>
+      </div>
+    </div>
+
+    {#if hasUnlockableContent}
+      <div class="flex flex-col gap-3" transition:slide>
+        <div class="text-xl">Add File</div>
+        <div class="text-sm	text-gray-500 flex justify-between">Format: JPG, PNG, GIF or PDF. Max size: 10MB.</div>
+        <FilePond
+          name="nftPrivateAssets"
+          onaddfile={handleAddFile}
+          acceptedFileTypes="image/png, image/jpeg, image/gif"
+          maxFileSize="10MB"
+          credits={false}
+        />
+      </div>
+    {/if}
+
     <div class="flex flex-col gap-4 sm:pt-4 pt-5">
       <Button variant="brand" type="submit" class="flex gap-2 text-2xl leading-6" disabled={$isSubmitting}>
         {#if $isSubmitting}
