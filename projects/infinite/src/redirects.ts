f910930646a852c3a4d02f2b9bc91f4548c8bc4a
Joel Del Cueto <wayfaringjou@gmail.com>
2022-04-01 10:54:45 +0200

    fix(app): disable about and cookies routes for infinite

--- projects/infinite/src/redirects.ts
+++ projects/infinite/src/redirects.ts
@@ -7,6 +7,18 @@ const redirects = ({
   route: string;
   status: 301 | 302;
   redirect: string;
-}[] => [];
+}[] => [
+  // Temporary redirect to disable /cookies and /about
+  {
+    route: '/cookies',
+    status: 301,
+    redirect: '/',
+  },
+  {
+    route: '/about',
+    status: 301,
+    redirect: '/',
+  },
+];
 
 export default redirects;
