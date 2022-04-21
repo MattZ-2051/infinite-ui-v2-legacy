97fbe1923664cedad23b44919cb99b0f620836e2
federico <falvarez@zircon.tech>
2022-04-21 14:29:03 -0300

    feat(app): add discord icon to the footer and preloading asset depending on the phase
    closes: #ci-659

--- projects/mclaren/src/Head.svelte
+++ projects/mclaren/src/Head.svelte
@@ -38,7 +38,13 @@
     crossorigin="anonymous"
   />
   {#if $page.url.pathname === '/'}
-    <link rel="preload" href={bucketAssets.landing.hero} as="video" />
+    <link
+      rel="preload"
+      href={$page.url.searchParams.get('phase') === '1' || $page.url.searchParams.get('phase') === '2'
+        ? bucketAssets.landing.hero
+        : bucketAssets.landing.hero3}
+      as="video"
+    />
   {/if}
   <link rel="stylesheet" href="/fonts/stylesheet.css" />
 </svelte:head>
--- projects/mclaren/src/footer/social-links.ts
+++ projects/mclaren/src/footer/social-links.ts
@@ -3,10 +3,12 @@ import facebook from './assets/facebook';
 import youtube from './assets/youtube';
 import instagram from './assets/instagram';
 import twitter from './assets/twitter';
+import discord from './assets/discord';
 
 export const links = [
   { id: 'Facebook', href: routes.facebook, path: facebook },
   { id: 'Twitter', href: routes.twitter, path: twitter },
   { id: 'Instagram', href: routes.instagram, path: instagram },
   { id: 'Youtube', href: routes.youtube, path: youtube },
+  { id: 'Discord', href: routes.discord, path: discord },
 ];
