f51d1f83899e7aa233113e0abb0a993b52c43e4d
Gabriel Ernesto Martínez Cánepa <contact@gemanepa.com>
2022-04-22 10:15:03 -0300

    feat(app): mclaren mobile header logo and sidebar icons adjustments (#1729)
    Closes CI-642

--- src/ui/sidebar-toggle/SidebarToggle.svelte
+++ src/ui/sidebar-toggle/SidebarToggle.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
+  import closeIcon from '$project/assets/lib/close-icon';
 
   export let isOpen: boolean;
 
   const dispatch = createEventDispatcher();
+
+  const sidebarIconColor = 'var(--mobile-sidebar-toggle-color, currentColor)';
 </script>
 
 <button type="button" class:open={isOpen} on:click={() => dispatch('toggle', !isOpen)} {...$$restProps}>
+  {#if !isOpen}
+    <svg>
+      <line y1="1" x2="32" y2="1" stroke-width="2" stroke={sidebarIconColor} />
+    </svg>
+    <svg>
+      <line y1="1" x2="32" y2="1" stroke-width="2" stroke={sidebarIconColor} />
+    </svg>
+    <svg>
+      <line y1="1" x2="32" y2="1" stroke-width="2" stroke={sidebarIconColor} />
+    </svg>
+  {:else}
+    <svg>
+      {#if typeof closeIcon.d === 'string'}
+        <path d={closeIcon.d} fill={sidebarIconColor} transform="scale(0.62,0.62)" />
+      {:else}
+        {#each closeIcon.d as d}
+          <path {d} stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke={sidebarIconColor} />
+        {/each}
+      {/if}
+    </svg>
+  {/if}
   <span>Menu</span>
 </button>
 
 <style lang="postcss">
   button {
     border: none;
-    width: var(--mobile-sidebar-toggle-icon-width, 30px);
-    height: 24px;
     padding: 0;
-    margin-top: var(--mobile-sidebar-toggle-icon-margin-top, 0);
     outline: none;
     position: relative;
     @apply z-menu;
-  }
-
-  button::before,
-  button::after,
-  button span {
-    background: var(--mobile-sidebar-toggle-color, currentColor);
   }
 
   button::before,
@@ -50,22 +65,24 @@
     transition: opacity 0.25s;
   }
 
-  button::before {
-    transform: translate3d(0, var(--mobile-sidebar-toggle-burger-icon-before, -10px), 0);
-  }
-
-  button::after {
-    transform: translate3d(0, var(--mobile-sidebar-toggle-burger-icon-after, 10px), 0);
-  }
   .open span {
     opacity: 0;
   }
 
-  .open::before {
-    transform: rotate3d(0, 0, 1, 45deg);
+  button svg {
+    width: var(--mobile-sidebar-toggle-icons-width, 30px);
   }
 
-  .open::after {
-    transform: rotate3d(0, 0, 1, -45deg);
+  button:not(.open) svg {
+    height: 2px;
+  }
+
+  button:not(.open) svg:not(:first-child) {
+    margin-top: var(--mobile-sidebar-burger-svg-line-margin-top, 8px);
+  }
+
+  button.open svg {
+    height: var(--mobile-sidebar-close-icon-height, 24px);
+    margin-top: var(--mobile-sidebar-close-icon-margin-top, 0);
   }
 </style>
