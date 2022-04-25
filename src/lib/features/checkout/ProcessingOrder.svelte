5110d153a027990976d827c3cf1608db363d8f55
Gabriel Ernesto Martínez Cánepa <contact@gemanepa.com>
2022-04-25 12:05:06 -0300

    fix(lib): not show 'go back to marketplace' option when marketplace is disabled (#1761)
    Closes CI-680

--- src/lib/features/checkout/ProcessingOrder.svelte
+++ src/lib/features/checkout/ProcessingOrder.svelte
@@ -4,6 +4,8 @@
   import routes from '$project/routes';
 
   export let etherscanLink = '';
+
+  const DISABLED_MARKETPLACE = import.meta.env?.VITE_DISABLE_MARKETPLACE === 'true';
 </script>
 
 <div class="flex flex-col xl:max-w-sm justify-center justify-self-center">
@@ -12,8 +14,12 @@
     >It can take up to 30 minutes for the order to process and the NFT to appear on your wallet.</span
   >
   <div class="mb-4 text-gray-700">
-    <span>You can wait for the order to process, close this window or</span>
-    <a class="underline underline-offset-4" href={routes.marketplace}>go back to the Marketplace.</a>
+    {#if DISABLED_MARKETPLACE}
+      <span>You can wait for the order to process or close this window</span>
+    {:else}
+      <span>You can wait for the order to process, close this window or</span>
+      <a class="underline underline-offset-4" href={routes.marketplace}>go back to the Marketplace.</a>
+    {/if}
   </div>
   <span class="mb-10 font-bold">We'll send you an email when your purchase has been completed.</span>
   <Button class="h-16" variant="brand" disabled><DualRingLoader /></Button>
