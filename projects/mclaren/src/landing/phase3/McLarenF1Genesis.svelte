b8ca2c6d7598675b30a453dbb03be8a92971bf85
Alejandro Alvarez <alealvarez14@gmail.com>
2022-04-25 16:22:11 +0200

    fix(app): mobile view on description

--- projects/mclaren/src/landing/phase3/McLarenF1Genesis.svelte
+++ projects/mclaren/src/landing/phase3/McLarenF1Genesis.svelte
@@ -36,11 +36,11 @@
         >
           MSO LAB <span class="block sm:inline">GENESIS COLLECTION</span>
         </p>
-        <div class="text-lg text-center font-light max-w-8xl leading-9 pb-10">
-          <p class="text-center text-base">
+        <div class="text-lg text-left md:text-center font-light max-w-8xl leading-9 pb-10">
+          <p class="text-base">
             An exclusive invitation-only Tier One membership mint marking McLaren Automotive's move into the metaverse.
           </p>
-          <p class="pt-10 text-center text-base">
+          <p class="pt-10 text-base">
             Unlock membership to MSO LAB through a pioneering collection of NFTs centred around one of the most
             desirable, most storied cars McLaren has ever produced – the McLaren P1™.
           </p>
