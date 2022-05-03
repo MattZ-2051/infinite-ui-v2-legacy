f95d93941f361a58b6368c9f1123a094751f2320
pedromauricio07 <pedrom.mauricio@gmail.com>
2022-05-03 22:25:28 +0100

    fix(app): fixing account settings update in mclaren

--- projects/mclaren/src/account/Account.svelte
+++ projects/mclaren/src/account/Account.svelte
@@ -50,7 +50,7 @@
       lastName: user.lastName || '',
       tagline: user.tagline || '',
       username: user.username || '',
-      phoneNumber: user.phoneNumber || '',
+      phoneNumber: user.phoneNumber,
       email: user.email || '',
     },
     onSubmit: async (values) => {
@@ -59,6 +59,7 @@
         let { username, ...rest } = values;
         values = rest;
       }
+      if (!values.phoneNumber) delete values.phoneNumber;
       try {
         submitting = true;
         await patchUser(values);
