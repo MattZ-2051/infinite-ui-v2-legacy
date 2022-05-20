535d22706b7ba7336fd8d5b1b81e21b31a3ea119
Joel Del Cueto <wayfaringjou@gmail.com>
2022-05-20 13:54:01 +0200

    fix(app): add eth disclaimer custom style for mclaren
    Closes CI-386

--- src/lib/features/sku/EthDisclaimer.svelte
+++ src/lib/features/sku/EthDisclaimer.svelte
@@ -8,10 +8,12 @@
   <div class="separator" />
   <div class="flex py-5 justify-between gap-2 items-center text-sm">
     <div class="flex gap-4 items-center">
-      <div class="rounded-full bg-default p-2">
+      <div class="rounded-full p-2" style="background-color: var(--eth-disclaimer-icon-color, var(--default-color))">
         <Icon path={ethereum} size="1" color="var(--bg-color)" />
       </div>
-      <span class="text-default">This {CLIENT_COMPANY_NAME} NFT is minted on Ethereum</span>
+      <span class="text-default" style="font-weight: var(--eth-disclaimer-weight, 400);"
+        >This {CLIENT_COMPANY_NAME} NFT is minted on Ethereum</span
+      >
     </div>
   </div>
   <div class="separator" />
