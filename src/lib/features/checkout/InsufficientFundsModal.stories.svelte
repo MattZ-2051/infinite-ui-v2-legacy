549e5d46112c0af8988f576229a31425c6c44d94
Simeon Aldana Vasquez <45675071+simealdana@users.noreply.github.com>
2022-05-20 14:11:39 -0300

    fix: created insufficient fund modal to checkout section (#2065)
    feat: resolved types issues
    
    feat: used text base for div
    
    feat: fix typo error
    
    Co-authored-by: simealdana <simealdana@gmail.com>

---
+++ src/lib/features/checkout/InsufficientFundsModal.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import InsufficientFundsModal from './InsufficientFundsModal.svelte';
+</script>
+
+<Meta title="Lib / features / Insufficient Funds Modal" component={InsufficientFundsModal} />
+
+<Template let:args>
+  <InsufficientFundsModal
+    totalCost="'100'"
+    insufficientFunds="'10'"
+    handleDeposit={() => {
+      alert('handleDeposit');
+    }}
+    handleOtherPaymentMethod={() => {
+      alert('handleOtherPaymentMethod');
+    }}
+  />
+</Template>
+
+<Story name="NotFundsModal" />
---
+++ src/lib/features/checkout/InsufficientFundsModal.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { Modal, closeModal } from '$ui/modals';
+  import Button from '$lib/components/Button.svelte';
+
+  export let title = 'Insufficient funds';
+  export let totalCost = '';
+  export let insufficientFunds = '';
+  export let handleDeposit: () => void;
+  export let handleOtherPaymentMethod: () => void;
+
+  const deposit = () => {
+    handleDeposit();
+    closeModal();
+  };
+
+  const selectOtherPaymentMethod = () => {
+    handleOtherPaymentMethod();
+    closeModal();
+  };
+</script>
+
+<Modal class="max-w-lg">
+  <svelte:fragment slot="title"><span class="font-medium text-2xl">{title}</span></svelte:fragment>
+  <div class="p-8">
+    <span class="text-base text-gray-500 font-medium"
+      >To complete your purchase you need to deposit more funds to your balance.</span
+    >
+  </div>
+  <div class="px-8 text-base font-medium">
+    <div class="flex justify-between pb-2">
+      <p>Total cost:</p>
+      <p class="text-lg">${totalCost}</p>
+    </div>
+    <div class="flex justify-between">
+      <p class="text-error">Insufficient funds:</p>
+      <p class="text-lg text-error">${insufficientFunds}</p>
+    </div>
+  </div>
+  <div class="flex flex-col p-8 text-base">
+    <Button variant="brand" class="mb-8" on:click={() => deposit()}>Deposit</Button>
+    <Button variant="outline-brand" on:click={() => selectOtherPaymentMethod()}>Select Another Payment Method</Button>
+  </div>
+</Modal>
