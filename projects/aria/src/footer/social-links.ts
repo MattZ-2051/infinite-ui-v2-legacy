7776f99a8ef29f921f22925ec4126b94179a3362
Kevin Quincke <kevinqf14@gmail.com>
2022-06-08 19:30:41 -0300

    fix(app): update discord link

--- projects/aria/src/footer/social-links.ts
+++ projects/aria/src/footer/social-links.ts
@@ -3,5 +3,5 @@ import { mdiTwitter, mdiInstagram, mdiDiscord } from '@mdi/js';
 export const links = [
   { id: 'Instagram', href: 'https://www.instagram.com/theariaexchange/', path: mdiInstagram },
   { id: 'Twitter', href: 'https://twitter.com/TheAriaExchange', path: mdiTwitter },
-  { id: 'Discord', href: 'https://discord.com/invite/3JtgwbduBt', path: mdiDiscord },
+  { id: 'Discord', href: 'https://discord.com/invite/8fjNHtY4Vx', path: mdiDiscord },
 ];
